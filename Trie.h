21608023fc916107f05551dbe5a14b79cdd62f61
mdn117 <48142070+mdn117@users.noreply.github.com>
2019-11-01 12:33:52 -0700

    implemented search function
    also implemented a helper function to incorporate recursion with the search function

--- Trie.h
+++ Trie.h
@@ -43,7 +43,61 @@ public:
 	vector<string> search(const string& word)
 	{
 		vector<string> matches;
+		TrieNode* current = _root;
+		string temp_word = word;
+		for (auto character : word)
+		{
+			if (current->hasChild(character))
+			{
+				current = current->getChild(character);
+			}
+			else
+			{
+				return matches;
+			}
+		}
+		for (auto child : current->getChildren())
+		{
+			if (child.first != '$')
+			{
+				temp_word = temp_word + child.first;
+				search_helper(matches, word + child.first);
+			}
+			else
+			{
+				matches.push_back(temp_word);
+			}
+		}
+
 		return matches;
+	}
+	void search_helper(vector<string>& matches, const string& word)
+	{
+		TrieNode* current = _root;
+		string temp_word = word;
+		for (auto character : word)
+		{
+			if (current->hasChild(character))
+			{
+				current = current->getChild(character);
+			}
+			else
+			{
+				return;
+			}
+		}
+		for (auto child : current->getChildren())
+		{
+			if (child.first != '$')
+			{
+				temp_word = temp_word + child.first;
+				search_helper(matches, word + child.first);
+			}
+			else
+			{
+				matches.push_back(temp_word);
+			}
+		}
 	}
 };
 
